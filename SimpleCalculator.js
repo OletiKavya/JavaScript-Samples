8f0410d5a3d42ab7b1e2e55a52506709f2e82b2d
KavyaOleti <ramanjulukavya@gmail.com>
2024-08-12 10:36:54 -0700

    Simple Calculator is Done.

---
+++ SimpleCalculator.js
@@ -0,0 +1,11 @@
+function calc(a,b){
+
+    console.log("The addition is ",a+b);
+    console.log("The subraction is ",a-b);
+    console.log("The Multiplication is ",a*b);
+    console.log("The Division  is ",a/b);
+
+}
+let x=8;
+let y=4;
+calc(x,y);
